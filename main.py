15e74ae0e9b16db885d603f8265fbae70afb28ff
JosephLiCodes <69280155+JosephLiCodes@users.noreply.github.com>
2021-12-11 20:10:30 -0800

    takes user input and outputs a prediction based on user in put data

--- main.py
+++ main.py
@@ -16,13 +16,14 @@ import pickle
 data = pd.read_csv("student-mat.csv", sep=";")
 print(data.head())
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
-print(data.head())
-
+#print(data.head())
+input = pd.read_csv("testdata.csv", sep=",")
+input = input[["G1", "G2", "studytime", "failures", "absences"]]
 predict = "G3"
-
+print(input)
 X = np.array(data.drop([predict],1)) #array with everything except for G3 (we are predicting G3)
 Y = np.array(data[predict]) #array with only g3 values
-
+input_array = np.array(input)
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1) #10 percent of data is going to be set aside to get tested for accurate results
 
 # TRAIN MODEL MULTIPLE TIMES FOR BEST SCORE
@@ -37,17 +38,19 @@ for _ in range(20):
     linear.fit(x_train, y_train)
     acc = linear.score(x_test, y_test)
     # If the current model has a better score than one we've already trained then save it
-    print(acc)
+    #print(acc)
     if acc > best:
         best = acc
         with open("studentgrades.pickle", "wb") as f:
             pickle.dump(linear, f)
 
 #load model with highest accuracy
-pickle_in = open("studentgrades.icpkle", "rb")
+pickle_in = open("studentgrades.pickle", "rb")
 linear = pickle.load(pickle_in)
+print("your predicted score out of 20 is: ", linear.predict(input_array))
 acc = linear.score(x_test, y_test) #accuracy of model
+
 print("used model accuracy: ", best)
 predictions = linear.predict(x_test)
-for x in range(len(predictions)):
-    print(predictions[x], x_test[x], y_test[x])
+#for x in range(len(predictions)):
+    #print(predictions[x], x_test[x], y_test[x])
